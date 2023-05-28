ae451248c0794d9e6d50246920f7ff24e024c130
fjroar <fjroar@hotmail.com>
2023-05-28 15:06:34 +0200

    Variation of Nutation function

---
+++ R/NutationLong.R
@@ -0,0 +1,75 @@
+#' @title NutationLong
+#'
+#' @description Estimation of the obliquity of ecliptic according to
+#' Chapter 22 of Astronomical Algorithms (second edition)
+#'
+#' @param julianCent Numerical vector which represents a Julian converted in millenium
+#'
+#' @return varNutation (Nutation in longitude), parameter needed for the Moon's Declination and Right Ascension
+#'
+#' @examples
+#'
+#' #JulianMil is computed at 12.04.1992, 00h 00m
+#' library(RMoon)
+#' varNutation <- NutationLong(juliancent = -0.077221081451)
+#'
+#' @references
+#' Jean Meeus (1991), Astronomical Algorithms, ISBN 0-943396-35-2
+#'
+#' @export
+
+NutationLong <- function(juliancent){
+
+  #Nutation in Longitude
+
+  D <- 297.85036 +
+    445267.111480 * juliancent -
+    0.0019142 * juliancent**2 +
+    juliancent**3 / 189474
+
+  D <- D %% 360
+  if (D < 0) {D <- D + 360}
+
+  M = 357.52772 + 35999.050340 * juliancent -
+    0.0001603 * juliancent**2 -
+    juliancent / 300000;
+
+  M <- M %% 360
+  if (M < 0) {M <- M + 360}
+
+  Mprime <- 134.96298 + 477198.867398 * juliancent +
+    0.0086972 * juliancent**2 +
+    juliancent**3 / 56250
+
+  Mprime <- Mprime %% 360
+  if (Mprime < 0) {Mprime <- Mprime + 360}
+
+  F_ <- 93.27191 + 483202.017538 * juliancent -
+    0.0036825 * juliancent**2 +
+    juliancent**3 / 327270;
+
+  F_ <- F_ %% 360
+  if (F_ < 0) {F_ <- F_ + 360}
+
+  O = 125.04452 - 1934.136261 * juliancent +
+    0.0020708 * juliancent**2 +
+    juliancent**3 / 450000;
+
+  O <- O %% 360
+  if (O < 0) {O <- O + 360}
+
+  df <- PeriodicNutObliq
+
+  df$ArgNutObliq <- pi * (df$D * D +
+    df$M * M +
+    df$M_ * Mprime +
+    df$F * F_ +
+    df$O * O) / 180
+
+  df$NutObliq <- (df$Nut1 + df$Nut2 * juliancent) *
+    0.0001 * sin(df$ArgNutObliq)
+
+  varNutation = sum(df$NutObliq)
+
+  return(varNutation)
+}
