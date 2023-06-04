46daa8f1413013a422f5b3c89a2e417ded2a7716
fjroar <fjroar@hotmail.com>
2023-06-04 14:29:18 +0200

    Correction

--- R/NutationLong.R
+++ R/NutationLong.R
@@ -27,37 +27,37 @@ NutationLong <- function(juliancent){
     #Nutation in Longitude
 
     D <- 297.85036 +
-      445267.111480 * juliancent -
-      0.0019142 * juliancent**2 +
-      juliancent**3 / 189474
+      445267.111480 * juliancent[i] -
+      0.0019142 * juliancent[i]**2 +
+      juliancent[i]**3 / 189474
 
     D <- D %% 360
     if (D < 0) {D <- D + 360}
 
-    M = 357.52772 + 35999.050340 * juliancent -
-      0.0001603 * juliancent**2 -
-      juliancent / 300000;
+    M = 357.52772 + 35999.050340 * juliancent[i] -
+      0.0001603 * juliancent[i]**2 -
+      juliancent[i] / 300000;
 
     M <- M %% 360
     if (M < 0) {M <- M + 360}
 
-    Mprime <- 134.96298 + 477198.867398 * juliancent +
-      0.0086972 * juliancent**2 +
-      juliancent**3 / 56250
+    Mprime <- 134.96298 + 477198.867398 * juliancent[i] +
+      0.0086972 * juliancent[i]**2 +
+      juliancent[i]**3 / 56250
 
     Mprime <- Mprime %% 360
     if (Mprime < 0) {Mprime <- Mprime + 360}
 
-    F_ <- 93.27191 + 483202.017538 * juliancent -
-      0.0036825 * juliancent**2 +
-      juliancent**3 / 327270;
+    F_ <- 93.27191 + 483202.017538 * juliancent[i] -
+      0.0036825 * juliancent[i]**2 +
+      juliancent[i]**3 / 327270;
 
     F_ <- F_ %% 360
     if (F_ < 0) {F_ <- F_ + 360}
 
-    O = 125.04452 - 1934.136261 * juliancent +
-      0.0020708 * juliancent**2 +
-      juliancent**3 / 450000;
+    O = 125.04452 - 1934.136261 * juliancent[i] +
+      0.0020708 * juliancent[i]**2 +
+      juliancent[i]**3 / 450000;
 
     O <- O %% 360
     if (O < 0) {O <- O + 360}
@@ -70,15 +70,12 @@ NutationLong <- function(juliancent){
                               df$F * F_ +
                               df$O * O) / 180
 
-    df$NutObliq <- (df$Nut1 + df$Nut2 * juliancent) *
+    df$NutObliq <- (df$Nut1 + df$Nut2 * juliancent[i]) *
       0.0001 * sin(df$ArgNutObliq)
 
     varNutation[i] = sum(df$NutObliq)/3600
 
-
-
   }
-
 
   return(varNutation)
 }
--- R/TrueObliquityEcliptic.R
+++ R/TrueObliquityEcliptic.R
@@ -24,7 +24,7 @@ TrueObliqutiyEcliptic <- function(juliancent){
 
   for (i in c(1: length(juliancent))){
 
-    U = juliancent / 100
+    U = juliancent[i] / 100
     epsilon0 <- 23 + 26 / 60 + 21.448 / 3600 -
       4680.93/ 3600 * U -
       1.55/ 3600  * U**2 +
@@ -40,37 +40,37 @@ TrueObliqutiyEcliptic <- function(juliancent){
     #Nutation in Obliquity
 
     D <- 297.85036 +
-      445267.111480 * juliancent -
-      0.0019142 * juliancent**2 +
-      juliancent**3 / 189474
+      445267.111480 * juliancent[i] -
+      0.0019142 * juliancent[i]**2 +
+      juliancent[i]**3 / 189474
 
     D <- D %% 360
     if (D < 0) {D <- D + 360}
 
-    M = 357.52772 + 35999.050340 * juliancent -
-      0.0001603 * juliancent**2 -
-      juliancent / 300000;
+    M = 357.52772 + 35999.050340 * juliancent[i] -
+      0.0001603 * juliancent[i]**2 -
+      juliancent[i] / 300000;
 
     M <- M %% 360
     if (M < 0) {M <- M + 360}
 
-    Mprime <- 134.96298 + 477198.867398 * juliancent +
-      0.0086972 * juliancent**2 +
-      juliancent**3 / 56250
+    Mprime <- 134.96298 + 477198.867398 * juliancent[i] +
+      0.0086972 * juliancent[i]**2 +
+      juliancent[i]**3 / 56250
 
     Mprime <- Mprime %% 360
     if (Mprime < 0) {Mprime <- Mprime + 360}
 
-    F_ <- 93.27191 + 483202.017538 * juliancent -
-      0.0036825 * juliancent**2 +
-      juliancent**3 / 327270;
+    F_ <- 93.27191 + 483202.017538 * juliancent[i] -
+      0.0036825 * juliancent[i]**2 +
+      juliancent[i]**3 / 327270;
 
     F_ <- F_ %% 360
     if (F_ < 0) {F_ <- F_ + 360}
 
-    O = 125.04452 - 1934.136261 * juliancent +
-      0.0020708 * juliancent**2 +
-      juliancent**3 / 450000;
+    O = 125.04452 - 1934.136261 * juliancent[i] +
+      0.0020708 * juliancent[i]**2 +
+      juliancent[i]**3 / 450000;
 
     O <- O %% 360
     if (O < 0) {O <- O + 360}
@@ -83,7 +83,7 @@ TrueObliqutiyEcliptic <- function(juliancent){
                               df$F * F_ +
                               df$O * O) / 180
 
-    df$NutObliq <- (df$Obl1 + df$Obl2 * juliancent) *
+    df$NutObliq <- (df$Obl1 + df$Obl2 * juliancent[i]) *
       0.0001 * cos(df$ArgNutObliq)
 
     varEpsilon = sum(df$NutObliq)
