282d1be7b85b0e6883018a5f287c53b72d3559ee
fjroar <fjroar@hotmail.com>
2024-04-26 23:39:10 +0200

    Eclipses prediction from Fred Espenak

---
+++ R/MoonEclipsesDates.R
@@ -0,0 +1,20 @@
+#' @title MoonEclipsesDates
+#'
+#' @description Dates of Lunar Eclipses since 3000 BC up to 3000 AC
+#'
+#' @docType data
+#'
+#' @usage This dataset can allow historic analysis year 0000 at original datasets here is -0001
+#'
+#' @format .rda
+#'
+#' @references Dates based on "Eclipse Predictions by Fred Espenak, www.EclipseWise.com"
+#'
+#' @keywords dataset
+#'
+#' @examples
+#'
+#' library(RMoon)
+#' df = MoonEclipsesDates
+#'
+"MoonEclipsesDates"
---
+++ R/SolarEclipsesDates.R
@@ -0,0 +1,20 @@
+#' @title SolarEclipsesDates
+#'
+#' @description Dates of Solar Eclipses since 3000 BC up to 3000 AC
+#'
+#' @docType data
+#'
+#' @usage This dataset can allow historic analysis year 0000 at original datasets here is -0001
+#'
+#' @format .rda
+#'
+#' @references Dates based on "Eclipse Predictions by Fred Espenak, www.EclipseWise.com"
+#'
+#' @keywords dataset
+#'
+#' @examples
+#'
+#' library(RMoon)
+#' df = SolarEclipsesDates
+#'
+"SolarEclipsesDates"
