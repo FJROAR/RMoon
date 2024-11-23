08a07ea085f96b00763e19afea09dd0c77c942c6
Francisco J Rodriguez Aragon <fjroar@hotmail.com>
2024-11-23 15:20:32 +0100

    Geographic data

---
+++ R/MoonGeograficData.R
@@ -0,0 +1,20 @@
+#' @title MoonGeographicData
+#'
+#' @description Moon geography of craters, monts, etc provide by the NASA
+#'
+#' @docType data
+#'
+#' @usage This dataset offer latitude and longitude data of visible and non visible face of the Moon
+#'
+#' @format .rda
+#'
+#' @references Dates extracted from NASA website https://www.nasa.gov
+#'
+#' @keywords dataset
+#'
+#' @examples
+#'
+#' library(RMoon)
+#' df = MoonGeographicData
+#'
+"MoonGeographicData"
